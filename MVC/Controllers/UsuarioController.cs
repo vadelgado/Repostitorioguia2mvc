9362c3a5da6a630e421ed60a849df17ac4515a47
vadelgado <vadelgado@udenar.edu.co>
2022-12-13 07:51:38 -0500

    Implementacion Boton Agregar MVC .NET

---
+++ MVC/Controllers/UsuarioController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+namespace MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        usuarioEntities db = new usuarioEntities();
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            var list = db.Persona.ToList();
+
+                return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create( Persona persona)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var dato = new Persona();
+                    dato.PrimerNombre = persona.PrimerNombre;
+                    dato.SegundoNombre = persona.SegundoNombre;
+                    dato.PrimerApellido = persona.PrimerApellido;
+                    dato.SegundoApellido = persona.SegundoApellido;
+                    dato.Ciudad = persona.Ciudad;
+                    dato.Edad = persona.Edad;
+
+                    db.Persona.Add(dato);
+                    db.SaveChanges();
+                    TempData["exito"] = "Registro agregado con éxito!";
+                    return RedirectToAction("Index");
+                }
+                return View(persona);
+            }
+            catch(Exception ex)
+            {
+                TempData["error"] = "Ha ocurrido un error al crear el registro!";
+                return RedirectToAction("Index");
+            }
+            
+        }
+    }
+}
---
+++ MVC/Models/ViewModels/PersonaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models.ViewModels
+{
+    public class PersonaViewModel
+    {
+        public int Id_usuario { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Ciudad { get; set; }
+        public Nullable<int> Edad { get; set; }
+    }
+}
